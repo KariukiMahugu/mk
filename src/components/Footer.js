407ec63de27603ab23b72d206047ab3adc4e9ada
Your Name <kariukimahugu@gmail.com>
2021-12-31 17:11:24 +0300

    Fixed deployment errors.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Footer.css';
 import FooterItem from './FooterItem';
